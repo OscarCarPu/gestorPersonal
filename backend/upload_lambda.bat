e3a65a4a5a54ed0682e551d189baf56175b6fb21
Oscar Carballo Puebla <oscar.pecas.cp@gmail.com>
2024-10-08 17:58:49 +0200

    separate insert process to api

--- backend/upload_lambda.bat
+++ backend/upload_lambda.bat
@@ -1,10 +1,17 @@
 @echo off
 
+:: Check if the index file name is provided
+if "%1"=="" (
+    echo Usage: %0 index_file_name
+    exit /b 1
+)
+
 :: Variables
 set S3_BUCKET_NAME=oscarcarballopuebla
+set INDEX_FILE_NAME=%1
 
 :: Zip the lambda function
-powershell Compress-Archive -Path backend/index.py -DestinationPath backend/index.zip
+powershell Compress-Archive -Path backend/%INDEX_FILE_NAME% -DestinationPath backend/index.zip
 
 :: Upload to S3
 aws s3 cp backend/index.zip s3://%S3_BUCKET_NAME%/gestorpersonal/backend/index.zip
